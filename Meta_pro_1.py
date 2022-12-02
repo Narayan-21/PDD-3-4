2a6ce2ebc79842c9f029ea1531369cc9b468194a
Narayan-21 <naaidjan.19@gmail.com>
2022-12-02 14:35:32 +0530

    Meta Programming

--- Meta_pro_1.py
+++ Meta_pro_1.py
@@ -4,7 +4,7 @@ class CustomType(type):
     def __new__(cls, name, base, class_dict):
         print('Customized type creation!')
         cls_obj = super().__new__(cls, name, base, class_dict)
-        cls_obj.circ = lambda self: 2*self.pi*self.r
+        cls_obj.circ = lambda self: 2*math.pi*self.r
         return cls_obj
 
 class_body = """
@@ -19,10 +19,22 @@ class_dict = {}
 
 exec(class_body, globals(), class_dict)
 
-Circle = CustomType('Circle', (), class_dict)
-print(Circle)
-type(Circle)
-print(isinstance(Circle, type))
+Circle_1 = CustomType('Circle', (), class_dict)
+print(Circle_1)
+type(Circle_1)
+print(isinstance(Circle_1, type))
 
-c = Circle(0,0,1)
-print(c.area())
+c_1 = Circle_1(0,0,1)
+print(c_1.area())
+
+class Circle_2(metaclass=CustomType):
+    def __init__(self, x, y, r):
+        self.x = x
+        self.y = y
+        self.r = r
+    def area(self):
+        return math.pi* self.r**2
+
+c_2 = Circle_2(0,0,2)
+print(c_2.area())
+print(c_2.circ())
